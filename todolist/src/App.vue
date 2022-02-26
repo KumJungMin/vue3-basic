1e3d2cb4d76dbdee85e55f06f454222c9f6a535d
KumJungMin <rmawjdals@naver.com>
2022-02-26 18:07:37 +0900

    study: 1. use setup function

---
+++ todolist/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="name">{{ greet }}</div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const name = "vue3";
+
+    // 2. 함수 정의
+    const greeting = (name) => {
+      return `hello ${name}`;
+    };
+    const greet = greeting(name);
+    return {
+      // 1. Obj에서 원하는 변수, 함수를 return
+      //  -> template에서 접근 가능한 변수, 함수
+      greet,
+    };
+  },
+};
+</script>
+
+<style>
+.name {
+  color: red;
+}
+</style>
