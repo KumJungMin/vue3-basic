39b3ed2dd9e1287e913ece55028c81ee98cf3399
KumJungMin <rmawjdals@naver.com>
2022-03-19 20:05:31 +0900

    chore: divide form component

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <div class="name">{{ name }}</div>
-  <input type="text" v-model="name" />
-  <!-- v-model: 변수 바인딩 + 변수 변경 이벤트 -->
-  <button class="btn btn-primary" @click="onClick">클릭</button>
+  <h2>To-Do List</h2>
+  <div class="container">
+    <TodoSimepleForm />
+    <div v-if="!todos.length">추가된 Todo가 없습니다.</div>
+    <div v-else class="card">
+      <div
+        class="card-body p-2 d-flex align-items-center"
+        v-for="(todo, idx) in todos"
+        :key="todo.id"
+      >
+        <div class="form-check flex-grow-1">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            v-model="todo.completed"
+          />
+          <label class="form-check-label" :class="{ todo: todo.completed }">{{
+            todo.subject
+          }}</label>
+        </div>
+        <div>
+          <button class="btn btn-danger btn-sm" @click="deleteTodo(idx)">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-// 양방향 바인딩하는 법
-import { ref } from "vue";
-export default {
-  setup() {
-    let name = ref("vue3");
+import { reactive } from "vue";
+import TodoSimepleForm from "./components/TodoSimpleForm.vue";
 
-    const onClick = () => {
-      name.value = "is awesome";
+export default {
+  components: { TodoSimepleForm },
+  setup() {
+    const todos = reactive([]);
+    const deleteTodo = (idx) => {
+      todos.splice(idx, 1);
     };
+
     return {
-      name,
-      onClick,
+      todos,
+      deleteTodo,
     };
   },
 };
 </script>
 
 <style>
-.name {
+.error {
   color: red;
+}
+.todo {
+  color: gray;
+  text-decoration: line-through;
 }
 </style>
---
+++ todolist/src/components/TodoSimpleForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <form class="d-flex" @submit.prevent="onSubmit">
+    <div class="flex-grow-1 mr-2">
+      <input
+        class="form-control"
+        v-model="todo"
+        type="text"
+        placeholder="Type new to-do"
+      />
+    </div>
+    <div>
+      <button class="btn btn-primary">Add</button>
+    </div>
+  </form>
+  <div v-show="hasError" class="error">This field cannot be empty</div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const todo = ref("");
+    const hasError = ref(false);
+    const onSubmit = () => {
+      if (todo.value === "") hasError.value = true;
+      else {
+        const newTodo = {
+          id: Date.now(),
+          subject: todo.value,
+          completed: false,
+        };
+      }
+    };
+
+    return { todo, hasError, onSubmit };
+  },
+};
+</script>
+
+<style></style>
